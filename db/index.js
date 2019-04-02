afa6dd3ccdc1fdc0351eb8709c568bc63786264c
anthonybo <twandeez@gmail.com>
2019-04-02 09:43:23 -0700

    Created the connection to the MySQL database locally

--- db/index.js
+++ db/index.js
@@ -3,7 +3,7 @@ const { dbConfig } = require('../config');
 
 const connection = mysql.createConnection(dbConfig);
 
-connection.connect(()=>{
+connection.connect((error)=>{
     if(error) {
         return console.log('Error connecting to MySQL DB: ', error.message);
     }
