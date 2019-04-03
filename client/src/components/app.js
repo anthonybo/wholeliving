a7700b3bb1a32848789deee7aaad25c84a8d7182
anthonybo <twandeez@gmail.com>
2019-04-03 16:30:55 -0700

    Completed setuping up the initial routing for later use with multiple maps

--- client/src/components/app.js
+++ client/src/components/app.js
@@ -2,15 +2,18 @@ import 'materialize-css/dist/css/materialize.min.css';
 import 'materialize-css/dist/js/materialize.min';
 import '../assets/css/app.scss';
 import React from 'react';
+import {Route, Switch, withRouter, Link} from 'react-router-dom';
 import MapContainer from './map/mapContainer';
 import WholeFoodsTable from './wholeFoodsTable/wholeFoodsTable';
+import Homepage from './homepage';
 import './app.scss';
 
 const App = () => (
-    <div>
+    <div className='application-container'>
         <h1 className='center white-text'>Whole Living</h1>
-        <MapContainer/>
-        <WholeFoodsTable/>
+        <Switch>
+            <Route exact path='/' component={Homepage}/>
+        </Switch>
     </div>
 );
 
