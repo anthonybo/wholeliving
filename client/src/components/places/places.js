fb72cd8e6c59fed5506156e38402b795f494ff1f
anthonybo <twandeez@gmail.com>
2019-04-16 09:32:38 -0700

    Completed initial setup for autocomplete for the location input field

--- client/src/components/places/places.js
+++ client/src/components/places/places.js
@@ -1,19 +1,22 @@
 import React, {Component} from 'react';
 import axios from "axios";
 import {withRouter} from 'react-router-dom';
+import ReactDOM from "react-dom";
 // import Places from "google-places-web";
 // import axios from 'axios';
 // const Places = require("google-places-web").default; // instance of GooglePlaces Class;
 // Places.apiKey = "AIzaSyD-NNZfs0n53D0caUB0M_ERLC2n9psGZfc";
 // Places.debug = __DEV__; // boolean;
-import M from "materialize-css";
+// import M from "materialize-css";
+import Cities from './cities';
 
 class Places extends Component {
 
     state = {
         keyword: '',
         location: '',
-        range: 10
+        range: 10,
+        new_location: ''
     }
 
     async getData(){
@@ -25,6 +28,7 @@ class Places extends Component {
         // });
 
         // console.log(data);
+        console.log('State At Push Time: ', this.state);
         if(this.state.keyword && this.state.location){
             // console.log('Props: ', this.props );
             this.props.history.push(`/crossReference/` + this.state.keyword + '/' + this.state.location + '/' + this.state.range);
@@ -32,34 +36,71 @@ class Places extends Component {
     }
 
     handleChange = (event) => {
+        // console.log('Setting Original State')
+        // console.log('New Location: ',this.refs.location.value);
+
         this.setState({
             [event.target.name]: event.target.value
         });
+
+        // console.log(this.state);
     }
 
     handleSubmit = (event) => {
         // console.log(event.target.form.clear());
+        console.log(this.refs.location.value);
+        var newLoc = this.refs.location.value;
+
+        if(this.refs.location.value !== ''){
+            console.log('Setting new state: ', newLoc);
+            this.setState({
+                location: newLoc,
+            }, ()=>{
+                console.log('Callback State?: ',this.state);
+                if(!this.state.keyword || !this.state.location){
+                    // alert('Please Complete Both Fields!');
+                    // M.toast({
+                    //     html: 'Please complete both fields!',
+                    //     displayLength: 2000,
+                    //     classes: 'pulse'
+                    // })
+                } else {
+                    this.props.history.push(`/crossReference/` + this.state.keyword + '/' + this.state.location + '/' + this.state.range);
+                }
+            });
+        }
+
         this.refs.keyword.value = '';
         this.refs.location.value = '';
         this.refs.range.value = '';
 
         // alert('A name was submitted: ' + this.state.keyword + ' ' + this.state.location);
         if(!this.state.keyword || !this.state.location){
-            alert('Please Complete Both Fields!');
+            // alert('Please Complete Both Fields!');
+            console.log('Here?');
+            M.toast({
+                html: 'Please complete both fields!',
+                displayLength: 2000,
+                classes: 'pulse'
+            })
         }
         event.preventDefault();
         this.getData();
     }
 
     componentDidMount() {
-        // this.getData();
-        // M.AutoInit();
+        const node = ReactDOM.findDOMNode(this);
+        let elems = null;
 
-        // M.updateData({
-        //     "Apple": null,
-        //     "Microsoft": null,
-        //     "Google": 'https://placehold.it/250x250'
-        // });
+        if (node instanceof HTMLElement) {
+            elems = node.querySelector('.autocomplete');
+        }
+        // var elems = document.querySelector('.autocomplete');
+        var instances = M.Autocomplete.init(elems, {minLength: 2, limit: 10});
+
+        instances.updateData(Cities);
+
+        // instances.open();
     }
 
     render(){
@@ -69,8 +110,9 @@ class Places extends Component {
                     <div className="col s4">
                             <input className='white-text' type="text" keyword="keyword" name='keyword' ref='keyword' onChange={this.handleChange} autoComplete='off' placeholder='search keyword'/>
                     </div>
-                    <div className="col s4">
-                            <input id="autocomplete-input" className='white-text autocomplete' type="text" location="location" name='location' ref='location' onChange={this.handleChange} autoComplete='off' placeholder='location'/>
+                    <div className="col s4 input-field">
+                        <i className="material-icons prefix">textsms</i>
+                        <input id="autocomplete-input" className='white-text autocomplete' type="text" location="location" name='location' ref='location' onChange={this.handleChange} autoComplete='off' placeholder='location'/>
                     </div>
                     <div className="col s3">
                         <input className='white-text' type="number" min="5" range="range" name='range' ref='range' onChange={this.handleChange} autoComplete='off' placeholder='miles'/>
